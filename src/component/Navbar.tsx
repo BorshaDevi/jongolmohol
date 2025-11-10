9e7f259e507e324b7b34abcef54bd9e013df63a2
Borsha Devi <borshadevi27@gmail.com>
2025-11-10 18:01:56 +0600

    added globals.css file button class

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,9 @@
+ const Navbar=()=>{
+    return (
+        <div>
+            <h1 className="font-bold">Navbar</h1>
+            <button className="button">Apply</button>
+        </div>
+    )
+}
+export default Navbar;
