e43782f190701d65fdc8cc1890fc02541ecb4c18
Borsha Devi <borshadevi27@gmail.com>
2025-11-12 18:28:56 +0600

    added home page class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/component/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <h1>Home page</h1>
       <Navbar></Navbar>
     </div>
