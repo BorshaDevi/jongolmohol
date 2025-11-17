b869c3e043bd24f9371789f7ac7dcf90fff8574e
Borsha Devi <borshadevi27@gmail.com>
2025-11-17 15:46:15 +0600

    added texts

--- src/component/AllDepartments.tsx
+++ src/component/AllDepartments.tsx
@@ -1,9 +1,18 @@
+type product={
+    id:number,
+    name:string,
+}
 const AllDepartments=()=>{
     return(
-        <div className="min-h-screen bg-green-700 w-1/2 rounded-sm shadow-md">
+        <div className="min-h-screen bg-white w-1/2 rounded-sm shadow-md">
              <div >
                 <div>
                     <h1 className="header text-xl">All Departments</h1>
+                    <div>
+                       {products.map(pro=>(
+                        <h1 key={pro.id}>{pro.name}</h1>
+                       ))}
+                    </div>
                 </div>
              </div>
 
@@ -11,3 +20,25 @@ const AllDepartments=()=>{
     )
 }
 export default AllDepartments;
+const products:product[]=[
+    {    
+        id:1,
+        name:'Fruit & Nut Gits'
+    },
+    {    
+        id:2,
+        name:'Fresh '
+    },
+    {    
+        id:3,
+        name:'Vegetables'
+    },
+    {    
+        id:4,
+        name:'Meat'
+    },
+    {    
+        id:5,
+        name:'Butter & Eggs'
+    },
+]
