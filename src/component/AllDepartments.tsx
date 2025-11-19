43ac2ac727ad3e799403eb0ce2e06ca162375815
Borsha Devi <borshadevi27@gmail.com>
2025-11-19 14:59:10 +0600

    added className in globals and alldepartment file

--- src/component/AllDepartments.tsx
+++ src/component/AllDepartments.tsx
@@ -8,10 +8,12 @@ const AllDepartments=()=>{
              <div >
                 <div>
                     <h1 className="header text-xl">All Departments</h1>
-                    <div>
+                    <div className="p-4 mt-8">
+                        <div className="space-y-2 ">
                        {products.map(pro=>(
-                        <h1 key={pro.id}>{pro.name}</h1>
+                        <h1 key={pro.id} className="department">{pro.name}</h1>
                        ))}
+                    </div>
                     </div>
                 </div>
              </div>
