3371af22fb278bf160f6c01c779b75c1da1fc08f
Borsha Devi <borshadevi27@gmail.com>
2025-12-29 22:21:09 +0600

    install hovercard avatar for navbar file

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,10 +1,15 @@
 'use client'
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-    const pathName=usePathname()
+    const pathName = usePathname()
+    const user = false;
+
     return (
         <div className="max-w-full h-max">
             <div>
@@ -17,42 +22,55 @@ const Navbar = () => {
                                 <NavigationMenu className="text-xl font-semibold">
                                     <NavigationMenuList >
                                         <NavigationMenuItem>
-                                        <Link href='/about' className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName==='/about'? 'text-orange-500':'text-black'}`}><h2>About</h2></Link>
+                                            <Link href='/about' className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName === '/about' ? 'text-orange-500' : 'text-black'}`}><h2>About</h2></Link>
                                         </NavigationMenuItem>
                                         <NavigationMenuItem>
-                                            <NavigationMenuTrigger className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName==='/nuts' || pathName==='/fish' || pathName==='/fruit' || pathName==='/vegetables'? 'text-orange-500':'text-black'}`}>All Departments</NavigationMenuTrigger>
+                                            <NavigationMenuTrigger className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName === '/nuts' || pathName === '/fish' || pathName === '/fruit' || pathName === '/vegetables' ? 'text-orange-500' : 'text-black'}`}>All Departments</NavigationMenuTrigger>
                                             <NavigationMenuContent className="bg-white opacity-85 ">
                                                 <ul className="grid gap-2 sm:w-[400px] md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                                                     <Link href='/nuts'>
-                                                    <li className="text-orange-500 hover:underline hover:underline-offset-8 ">Nuts</li>
-                                                    <p className="text-gray-500 ">All types of  Nuts you can get here. </p>
+                                                        <li className="text-orange-500 hover:underline hover:underline-offset-8 ">Nuts</li>
+                                                        <p className="text-gray-500 ">All types of  Nuts you can get here. </p>
                                                     </Link>
-                                                    
+
                                                     <Link href='/fish'>
-                                                    <li className="text-orange-500 hover:underline hover:underline-offset-8">Fish</li>
-                                                    <p className="text-gray-500 ">All types of Fish you can get here. </p>
+                                                        <li className="text-orange-500 hover:underline hover:underline-offset-8">Fish</li>
+                                                        <p className="text-gray-500 ">All types of Fish you can get here. </p>
                                                     </Link>
                                                     <Link href='/fruit'>
-                                                    <li className="text-orange-500 hover:underline hover:underline-offset-8"> Fruit</li>
-                                                    <p className="text-gray-500 ">All types of Fruit you can get here. </p>
+                                                        <li className="text-orange-500 hover:underline hover:underline-offset-8"> Fruit</li>
+                                                        <p className="text-gray-500 ">All types of Fruit you can get here. </p>
                                                     </Link>
                                                     <Link href='/vegetables'>
-                                                    <li className="text-orange-500 hover:underline hover:underline-offset-8">Vegetables</li>
-                                                    <p className="text-gray-500 ">All types of Vegetables you can get here. </p>
+                                                        <li className="text-orange-500 hover:underline hover:underline-offset-8">Vegetables</li>
+                                                        <p className="text-gray-500 ">All types of Vegetables you can get here. </p>
                                                     </Link>
                                                 </ul>
                                             </NavigationMenuContent>
                                         </NavigationMenuItem>
                                         <NavigationMenuItem>
-                                        <Link href='/blog' className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName==='/blog'? 'text-orange-500':'text-black'}`}><h2>Blog</h2></Link>
+                                            <Link href='/blog' className={`text-xl font-semibold hover:underline hover:underline-offset-8 ${pathName === '/blog' ? 'text-orange-500' : 'text-black'}`}><h2>Blog</h2></Link>
                                         </NavigationMenuItem>
                                     </NavigationMenuList>
                                 </NavigationMenu>
-
-
                             </div>
                             {/* Mobile */}
-                            <p>login</p>
+                            {user ?
+                                <HoverCard>
+                                    <HoverCardTrigger>
+                                        <Avatar>
+                                            <AvatarFallback className='hover:cursor-pointer'>CN</AvatarFallback>
+                                        </Avatar>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent className="bg-white opacity-85 ">
+                                        <div className="grid grid-cols-1 text-xl hover:cursor-pointer">
+                                            <Link href='/dashboard' >Dashboard</Link>
+                                            <Button className='mt-3 text-xl hover:cursor-pointer'>Logout</Button>
+                                        </div>
+                                    </HoverCardContent>
+                                </HoverCard>
+
+                                : <Link href='/login' className="text-xl text-white font-semibold bg-orange-500 p-2 rounded-md">Join us</Link>}
                         </div>
                     </div>
 
