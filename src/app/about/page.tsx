903b8fedcf078ca7c47eebc3399f619a6bcdc963
Borsha Devi <borshadevi27@gmail.com>
2026-01-07 21:21:13 +0600

    create About.tsx ,Blog.tsx component and page folder

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import About from "@/component/pages/About"
+
+const AboutPage=()=>{
+    return(
+        <div>
+              <About></About>
+        </div>
+    )
+}
+export default  AboutPage
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Blog from "@/component/pages/Blog"
+
+const BlogPage=()=>{
+    return(
+        <div>
+              <Blog></Blog>
+        </div>
+    )
+}
+export default  BlogPage
