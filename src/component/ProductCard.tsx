f950018dce34fef53f953c095eebe8bd6968e6fd
Borsha Devi <borshadevi27@gmail.com>
2026-01-24 23:04:13 +0600

    add text in Fish.tsx page and also create viewCard ,[id] page for product full detail to see

--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -1,49 +1,53 @@
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import toast, { Toaster } from 'react-hot-toast';
+import Link from "next/link";
 
 
-type ProductCardProps={
-    image:string,
-    name:string,
-    price:number, 
+
+type ProductCardProps = {
+    image: string,
+    name: string,
+    price: number,
+    id:string,
 }
-const ProductCard=({image,name,price}:ProductCardProps)=>{
-    const handleAdd=()=>{
-        toast.success(`${name} is added`)
-    }
-    return(
-         <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
-      {/* Product Image */}
-      <div className="relative w-full h-40">
-        <Image
-          src={image}
-          alt={name}
-          fill
-          className="object-cover"
-        />
-      </div>
+const ProductCard = ({ image, name, price ,id}: ProductCardProps) => {
+    // const handleAdd = () => {
+    //     toast.success(`${name} is added`)
+    // }
+    // <Toaster
+    //             position="top-center"
+    //         />
+    // import toast, { Toaster } from 'react-hot-toast';
+    return (
+        <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+            {/* Product Image */}
+            <div className="relative w-full h-40">
+                <Image
+                    src={image}
+                    alt={name}
+                    fill
+                    className="object-cover"
+                />
+            </div>
 
-      {/* Product Info */}
-      <div className="p-3 flex flex-col flex-1">
-        <h3 className="text-sm font-semibold">{name}</h3>
-        <p className="text-sm text-gray-500 mt-1">{price}</p>
-      </div>
+            {/* Product Info */}
+            <div className="p-3 flex flex-col flex-1">
+                <h3 className="text-2xl font-semibold">{name}</h3>
+                <p className="text-xl text-red-500 mt-1">{price}/kg</p>
+            </div>
 
-      {/* Add Button */}
-      <div className="p-3 pt-0">
-        <Button
-          onClick={handleAdd}
-          className="w-full"
-          size="sm"
-        >
-          Add
-        </Button>
-      </div>
-      <Toaster
-      position="top-center"
-      />
-    </div>
+            {/* Add Button */}
+            <div className="p-3 pt-0">
+                <Button
+                    asChild
+                    className="bg-green-900 text-white rounded-md text-xl font-semibold p-1 w-full hover:bg-green-700" 
+                >
+                    <Link href={`/viewCard/${id}`}>View Card</Link>
+                </Button>
+            </div>
+            
+        </div>
     )
 }
 export default ProductCard;
--- src/component/pages/Fish.tsx
+++ src/component/pages/Fish.tsx
@@ -1,10 +1,79 @@
+'use client'
+import ProductCard from "../ProductCard";
 import ShareBanner from "../ShareBanner";
 
-const Fish=()=>{
+const fishList = [
+    {
+        id: "1",
+        name: "Rui Fish",
+        price: 350,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "2",
+        name: "Katla Fish",
+        price: 400,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "3",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "4",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "5",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "6",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id:" 7",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "8",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+    {
+        id: "9",
+        name: "Hilsha Fish",
+        price: 900,
+        image: "https://res.cloudinary.com/dg1okcxsx/image/upload/v1767275901/fresh-bass-with-white-background_xp9ts1.jpg",
+    },
+]
+const Fish = () => {
     return (
         <div>
             <ShareBanner title={"Fish collection"} text={'Here are various types of Fish.'} image={'https://res.cloudinary.com/dg1okcxsx/image/upload/v1769254427/overhead-shot-colorful-koi-fish-gathered-all-together-water_tp4htl.jpg'}></ShareBanner>
-            <h1>Fish</h1>
+
+            <div className="p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+                {fishList.map(fish => (
+                    <ProductCard
+                        key={fish.id}
+                        image={fish.image}
+                        name={fish.name}
+                        price={fish.price}
+                    />
+                ))}
+            </div>
+
         </div>
     )
 }
