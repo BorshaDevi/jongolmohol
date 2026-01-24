4b58549588320e7748c00ad131f368553bcb2a73
Borsha Devi <borshadevi27@gmail.com>
2026-01-24 17:15:56 +0600

    add text in the ShareBanner.tsx file to share nuts,fish,vegetables,fruit page.It's helps to find easlie 1 component

--- src/component/ShareBanner.tsx
+++ src/component/ShareBanner.tsx
@@ -1,8 +1,29 @@
+import Image from "next/image";
+
 const ShareBanner=()=>{
     return(
-        <div>
-            
-        </div>
+       <div className="relative w-full h-[220px] rounded-xl overflow-hidden">
+      {/* Image */}
+      <Image
+        src="/banner.jpg"   // public/banner.jpg
+        alt="Share Banner"
+        fill
+        className="object-cover"
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/50" />
+
+      {/* Text Content */}
+      <div className="absolute inset-0 flex flex-col justify-end p-4 text-white">
+        <h2 className="text-xl font-semibold">
+          Share with friends
+        </h2>
+        <p className="text-sm opacity-90">
+          Invite your friends and earn rewards
+        </p>
+      </div>
+    </div>
     )
 }
 export default ShareBanner;
